bea1c4a59ae346f8c3d5bf09111599025c889876
challonsy <musain2019@naver.com>
2025-05-24 18:28:05 +0900

    feat : cors 세팅 1

--- src/main/java/ceos/study/vote/global/config/SecurityConfig.java
+++ src/main/java/ceos/study/vote/global/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors-> cors.configurationSource(CorsConfig.corsConfigurationSource()))
                 .csrf(CsrfConfigurer::disable)
                 .formLogin((auth) -> auth.disable())
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
